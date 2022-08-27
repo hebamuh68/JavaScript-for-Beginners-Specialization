151f077b875fffd9360b391480bd57a3fdd0618b
Heba Allah. Hashim <69214737+hebamuh68@users.noreply.github.com>
2022-08-27 21:19:52 +0200

    Add files via upload

---
+++ m4.3---Vacation-Destination-Part1---START/script.js
@@ -0,0 +1,86 @@
+(function(){
+
+
+var detailsForm = document.querySelector('#destination_details_form');
+
+detailsForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var destName = event.target.elements['name'].value;
+    var destLocation = event.target.elements['location'].value;
+    var destPhoto = event.target.elements['photo'].value;
+    var destDesc = event.target.elements['description'].value;
+
+    for (var i = 0; i < detailsForm.length; i++) {
+        detailsForm.elements[i].value = "";
+    }
+
+
+    var destCard = createDestinationCard(destName, destLocation, destPhoto, destDesc);
+
+    var wishListContainer = document.getElementById('destination_container');
+
+    if (wishListContainer.children.length === 0) {
+        document.getElementById('title').innerHTML = "My Wish List";
+    }
+
+    document.querySelector('#destination_container').appendChild(destCard);
+}
+
+
+function createDestinationCard(name, location, photoURL, description) {
+
+    var card = document.createElement('div');
+    card.className = 'card';
+
+    var img = document.createElement('img');
+    img.setAttribute('alt', name);
+
+    var constantPhotoUrl = 'images/signpost.jpg';
+
+    if (photoURL.length === 0) {
+        img.setAttribute('src', constantPhotoUrl);
+    } 
+    else {
+        img.setAttribute('src', photoURL);
+    }
+
+    card.appendChild(img)
+
+    var cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    var cardTitle = document.createElement('h3');
+    cardTitle.innerText = name;
+    cardBody.appendChild(cardTitle);
+
+    var cardSubtitle = document.createElement('h4');
+    cardSubtitle.innerText = location;
+    cardBody.appendChild(cardSubtitle);
+
+    if (description.length !== 0) {
+        var cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.innerText = description;
+        cardBody.appendChild(cardText);
+    }
+
+    var cardDelteBtn = document.createElement('button');
+    cardDelteBtn.innerText = 'Remove';
+
+    cardDelteBtn.addEventListener('click', removeDestination);
+    cardBody.appendChild(cardDelteBtn);
+
+    card.appendChild(cardBody);
+    return card;
+
+}
+
+function removeDestination(event) {
+    var card = event.target.parentElement.parentElement;
+    card.remove();
+}
+
+})();
